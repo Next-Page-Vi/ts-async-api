c070123f1cc0281a6eb4b79e6f78ba60607c7857
plusls <plusls@qq.com>
2025-08-01 23:52:08 +0800

    feat: 合并多余的频道切换事件

--- example/notify-bot.py
+++ example/notify-bot.py
@@ -1,12 +1,13 @@
 """ts server query client logic"""
-
+import asyncio
 import logging
+from asyncio import Task
 from typing import Literal, Optional
 
 from pydantic import Field
 from pydantic_settings import BaseSettings, CliApp
 
-from ts_async_api.server_query.client import Client
+from ts_async_api.server_query.client import Client, ServerStatus
 from ts_async_api.server_query.event import EventBase
 from ts_async_api.server_query.event.notifycliententerview import ClientEnterEvent
 from ts_async_api.server_query.event.notifyclientleftview import ClientLeftEventBase
@@ -43,30 +44,74 @@ class AsyncSettings(BaseSettings, cli_enforce_required=True):
             await client.login(self.username, self.password)
             await client.use(1, virtual=True, client_nickname=self.client_nickname)
             await client.listen_all_event()
+            ctx = ClientStatusChangeEventCtx(server_status=client.server_status)
 
-            client.event_manager.register(ClientEnterEvent, client_enter_server_callback)
+            client.event_manager.register(ClientEnterEvent, ctx.client_enter_server_callback)
             client.event_manager.register(ClientLeftEventBase, client_left_server_callback)
-            client.event_manager.register(ClientMovedEventBase, client_moved_callback)
+            client.event_manager.register(ClientMovedEventBase, ctx.client_moved_callback)
             # 需要调用 wait, 不然里头的 task 出异常了不会向外抛出
             await client.wait()
 
 
-async def client_enter_server_callback(client: Client, event: EventBase) -> bool:
-    """Enter server callback"""
-    assert isinstance(event, ClientEnterEvent)
-    # 可以直接从 server_status 获取对应的信息, 只要执行过程中没有 await
-    # await 后不保证还能获取到对应的信息 (因为可能客户端会退出)
+EVENT_MERGE_TIME = 5.0
 
-    client_info = client.server_status.client_list[event.clid]
-    LOGGER.info(
-        "用户 %s 加入了服务器, ip: %s, 频道: %s, 客户端版本: %s",
-        client_info.client_nickname,
-        client_info.connection_client_ip,
-        client.server_status.channel_list[client_info.cid].channel_name,
-        client_info.client_version,
-    )
-    return False
+class ClientStatusChangeEventCtx:
+    """event ctx"""
 
+    # cid -> event
+    event_map: dict[int,ClientEnterEvent|ClientMovedEventBase]
+    server_status: ServerStatus
+    background_tasks: set[Task[None]]
+
+    def __init__(self, server_status: ServerStatus) -> None:
+        self.event_map = {}
+        self.server_status = server_status
+        self.background_tasks = set()
+
+    async def report_event(self, event: ClientEnterEvent|ClientMovedEventBase) -> None:
+        """Report event"""
+        await asyncio.sleep(EVENT_MERGE_TIME)
+        if isinstance(event, ClientEnterEvent):
+            client_info = self.server_status.client_list.get(event.clid)
+            if client_info is not None:
+                LOGGER.info(
+                    "用户 %s 加入了服务器, ip: %s, 频道: %s, 客户端版本: %s",
+                    client_info.client_nickname,
+                    client_info.connection_client_ip,
+                    self.server_status.channel_list[client_info.cid].channel_name,
+                    client_info.client_version,
+                )
+        else:
+            client_info = self.server_status.client_list.get(event.clid)
+            if client_info is not None:
+                LOGGER.info(
+                    "用户 %s 从频道 %s 切换到频道 %s",
+                    client_info.client_nickname,
+                    self.server_status.channel_list[event.cfid].channel_name,
+                    self.server_status.channel_list[event.ctid].channel_name,
+                )
+        del self.event_map[event.clid]
+
+
+    async def client_enter_server_callback(self, client: Client, event: EventBase) -> bool:  # noqa: ARG002
+        """Enter server callback"""
+        assert isinstance(event, ClientEnterEvent)
+        if event.clid not in self.event_map:
+            self.event_map[event.clid] = event
+            task = asyncio.create_task(self.report_event(event))
+            self.background_tasks.add(task)
+            task.add_done_callback(self.background_tasks.discard)
+        return False
+
+    async def client_moved_callback(self, client: Client, event: EventBase) -> bool:  # noqa: ARG002
+        """Move channel callback"""
+        assert isinstance(event, ClientMovedEventBase)
+        if event.clid not in self.event_map:
+            self.event_map[event.clid] = event
+            task = asyncio.create_task(self.report_event(event))
+            self.background_tasks.add(task)
+            task.add_done_callback(self.background_tasks.discard)
+        return False
 
 async def client_left_server_callback(client: Client, event: EventBase) -> bool:
     """Left server callback"""
@@ -78,20 +123,6 @@ async def client_left_server_callback(client: Client, event: EventBase) -> bool:
         client.server_status.channel_list[client_info.cid].channel_name,
     )
     return False
-
-
-async def client_moved_callback(client: Client, event: EventBase) -> bool:
-    """Move channel callback"""
-    assert isinstance(event, ClientMovedEventBase)
-    client_info = client.server_status.client_list[event.clid]
-    LOGGER.info(
-        "用户 %s 从频道 %s 切换到频道 %s",
-        client_info.client_nickname,
-        client.server_status.channel_list[event.cfid].channel_name,
-        client.server_status.channel_list[event.ctid].channel_name,
-    )
-    return False
-
 
 if __name__ == "__main__":
     CliApp.run(AsyncSettings)
